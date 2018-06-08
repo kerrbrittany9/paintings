ed2ad060c36407eb755ff7ffca9b58748b036dae
brittany hardison <kerrbrittany9@gmail.com>
2018-06-08 16:33:23 -0700

    add drawings link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Header/Header";
 import Menew from "./Menu/Menew";
 import About from './About/About';
 import Gallery from  './Gallery/Gallery';
+import Drawings from  './Gallery/Drawings';
 import Texas from './Gallery/Texas';
 import styles from './styles.css';
 import { Switch, Route } from 'react-router-dom';
@@ -13,17 +14,17 @@ class App extends React.Component {
     return (
       <div id="outer-container">
         <div className = {styles.body}>
-          
-          <Menew />
-          <main id="page-wrap">
-            <Header />
-          </main>
-          <Switch>
-            <Route exact path="/about" component={About} />
-            <Route path="/gallery" component={Gallery} />
-            <Route path="/Texas" component={Texas} />
-            <Route path="/#/" component={App} />
-          </Switch>
+           <Menew />
+            <main id="page-wrap">
+              <Header />
+            </main>
+            <Switch>
+              <Route exact path="/about" component={About} />
+              <Route path="/gallery" component={Gallery} />
+              <Route path="/Texas" component={Texas} />
+              <Route path="/Drawings" component={Drawings} />
+              <Route path="/#/" component={App} />
+            </Switch>
         </div>
       </div>
     );
