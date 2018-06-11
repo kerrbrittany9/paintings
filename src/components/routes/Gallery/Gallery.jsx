31080d6d1bc7fe77637256302971db1105f15b3e
brittany hardison <kerrbrittany9@gmail.com>
2018-06-11 11:25:03 -0700

    add header component to each route

--- src/components/routes/Gallery/Gallery.jsx
+++ src/components/routes/Gallery/Gallery.jsx
@@ -3,11 +3,13 @@ import styles from "./styles.css";
 import PropTypes from "prop-types";
 import Texas from "./Texas";
 import Drawings from "./Drawings";
+import Header from '../../Header/Header';
 import { Link } from "react-router-dom";
 
 function Gallery(props) {
   return (
     <div className={styles.header}>
+      <Header />
       <Link className={styles.link} to="/Texas">
         {" "}
         let's see texas
