f5f1703ebaa67426b8c4914d45573c92e9fbc9ac
brittany hardison <kerrbrittany9@gmail.com>
2018-06-20 14:06:04 -0700

    remove texas as a link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,20 +20,14 @@ class App extends React.Component {
     }
   }
 
-  // This keeps your state in sync with the opening/closing of the menu
-  // via the default means, e.g. clicking the X, pressing the ESC key etc.
   handleStateChange(state) {
     this.setState({ menuOpen: state.isOpen })
   }
 
-  // This can be used to close the menu, e.g. when a user clicks a menu item
   closeMenu() {
     this.setState({ menuOpen: false })
   }
 
-  // This can be used to toggle the menu, e.g. when using a custom icon
-  // Tip: You probably want to hide either/both default icons if using a custom icon
-  // See https://github.com/negomi/react-burger-menu#custom-icons
   toggleMenu() {
     this.setState({ menuOpen: !this.state.menuOpen })
   }
@@ -88,7 +82,6 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/about" component={About} />
           <Route path="/gallery" component={Gallery} />
-          {/* <Route path="/Texas" component={Texas} /> */}
           <Route path="/" component={Home} />
         </Switch>
         <main id="page-wrap">
