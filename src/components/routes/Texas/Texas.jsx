ea48d0bf07414e97bca7e4e48203088e172e6783
brittany hardison <kerrbrittany9@gmail.com>
2018-08-09 16:34:20 -0700

    add slider to page

--- src/components/routes/Texas/Texas.jsx
+++ src/components/routes/Texas/Texas.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-// import styles from './styles.css';
 import PropTypes from 'prop-types';
 import styles from "./styles.css";
 import Header from '../../Header/Header';
+import Slider from 'react-slick';
+import mark from './assets/mark.jpg';
+import vincent from './assets/vincent.jpg';
 
 
-// import Slider from 'react-slick';
-// import asti from './assets/texas/asti1.jpg';
-// import bartonsprings from './assets/texas/bartonsprings.jpg';
 // import birdsonwires from './assets/texas/birdsondemwires.jpg';
 // import bridge from './assets/texas/bridge1.jpg';
 
@@ -15,27 +14,29 @@ function Texas(props) {
   return (
     <div>
       <Header />
-      {/* <Slider
-       dots
-       centerMode={true}
-       infinite
-       autoplay
-       speed={500}
-       slidesToShow={1}
-       slidesToScroll={1}
-       fade
-       cssEase="linear"
-       className="slick-slider-fade"
-      >
-        <div className = {styles.slider}><img src={asti} alt="Art" /></div>
-        <div className = {styles.slider}><img src={bartonsprings} alt="Things" /></div>
-        <div className = {styles.slider}><img src={birdsonwires} alt="STuff" /></div>
-      </Slider>
-      <div className = {styles.body}>
-        <img className = {styles.image} src={bridge} alt="bridge"/>
-        <img className = {styles.image} src={bridge} alt="bridge"/>
-      </div> */}
-      <h1 >this is texas</h1>
+      <div className={styles.texas}>
+        <p>welcome to texas</p>
+        <Slider
+        dots
+        centerMode={true}
+        infinite
+        autoplay
+        speed={500}
+        slidesToShow={1}
+        slidesToScroll={1}
+        fade
+        cssEase="linear"
+        className="slick-slider-fade"
+
+        >
+          <div><img src={mark} alt="Art" /></div>
+          <div><img src={vincent} alt="Things" /></div>
+        </Slider>
+        <div className={styles.insert}>
+          <img src={vincent} alt="vincent"/>
+          <img src={vincent} alt="vincent"/>
+      </div>
+    </div>
   </div>
   );
 }
